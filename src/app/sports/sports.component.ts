252d15f8e425ec1a85d7d5eebf57c3c46742a452
mia-moran <100383520@alumnos.uc3m.es>
2020-11-18 22:30:10 +0100

    Firebase funciona !!!!!

--- src/app/sports/sports.component.ts
+++ src/app/sports/sports.component.ts
@@ -13,25 +13,25 @@ export class SportsComponent implements OnInit {
     {
       'name':'NATACIÓN', 
       'img':"../../assets/stock/deporte1.jpg", 
-      'description':'Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum', 
+      'description':'Al agua patos! En Leganés y Getafe', 
       'link': ''
     },
     {
       'name':'CICLISMO', 
       'img':"../../assets/stock/bicicleta.jpeg", 
-      'description':'Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum', 
+      'description':'Rutas todos los fines de semana', 
       'link': ''
     },
     {
       'name':'RUNNING', 
       'img':"../../assets/stock/running.jpeg", 
-      'description':'Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum', 
+      'description':'A correr se ha dicho ! Preparamos maratones', 
       'link': ''
     },
     {
       'name':'TENIS', 
       'img':"../../assets/stock/tenis.jpeg", 
-      'description':'Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum Loreipsum', 
+      'description':'Nadal, Federer y los cracks de la UC3M', 
       'link': ''
     },
   ]
